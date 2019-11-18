59a2918fe1f69b652355d36bfdcebe6a38e78610
Remy Coquard <remy.cqrd@gmail.com>
2019-11-18 21:47:25 +0100

    Adding the 3 polylines on the map, some issues here

--- app/components/path-editor.js
+++ app/components/path-editor.js
@@ -10,7 +10,9 @@ export default Component.extend({
   lng: -0.552049,
   zoom: 15,
   // Ember array containing the polyline to be displayed on top of the map
-  polyline: A([]),
+  polylineRecommended: A([]),
+  polylineFastest: A([]),
+  polylineShortest: A([]),
   // Boolean indicating if the target for next action is the starter marker or a polyline
   firstClick: false,
   lastClick: false,
@@ -23,13 +25,13 @@ export default Component.extend({
   firstAddressLat: 0,
   firstAddressLon: 0,
 
-  preference: "fastest",
+  preference: "recommended",
   profile: "cycling-regular",
 
   tileUrl: "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
 
   isEnabled: computed('polyline.@each.lat', 'polyline.@each.lon', function () {
-    if (this.get('polyline').length > 1){
+    if (this.get('polylineRecommended').length > 1){
       return false;
     } else {
       return true;
@@ -53,15 +55,11 @@ export default Component.extend({
     return Math.round(this.get('duration')/60) + ' min';
   }),
 
-  startPoint: computed('polyline', function () {
-    return this.get('polyline').get('firstObject');
+  startPoint: computed('polylineRecommended', function () {
+    return this.get('polylineRecommended').get('firstObject');
   }),
-
-  endPoint: computed('polyline', function () {
-    return this.get('polyline').get('lastObject');
-  }),
-  middlePoint: computed('polyline', function () {
-    return this.get('polyline').objectAt(Math.round(this.get('polyline').length/2));
+  endPoint: computed('polylineFastest', function () {
+    return this.get('polylineFastest').get('lastObject');
   }),
 
   actions: {
@@ -73,7 +71,9 @@ export default Component.extend({
     },
 
     deleteAll() {
-      this.set("polyline", A([]));
+      this.set("polylineRecommended", A([]));
+      this.set("polylineFastest", A([]));
+      this.set("polylineShortest", A([]));
       this.set('firstClick', false);
       this.set('lastClick', false);
       this.set('firstAddress', "");
@@ -111,14 +111,14 @@ export default Component.extend({
 
     pathCreation(latitude, longitude, preference, profile, polyline){
       let ctx = this;
-      let prevLat = this.get('polyline').objectAt(ctx.get('polyline').length-1).lat;
-      let prevLon = this.get('polyline').objectAt(ctx.get('polyline').length-1).lon;
+      let prevLat = this.get(polyline).objectAt(ctx.get(polyline).length-1).lat;
+      let prevLon = this.get(polyline).objectAt(ctx.get(polyline).length-1).lon;
       // Calling ORS for a polyline segment joining the given two coordinates
       let url = "/ors/routes/?profile=" + profile + "&coordinates="+prevLon+","+prevLat+"|"+longitude+","+latitude+"&format=geojson&preference=" + preference + "&language=fr";
       fetch(url)
         .then(function(response) { return response.json(); })
         .then(function(data){
-          ctx.get('previousIndex').push(ctx.get('polyline').length+1);
+          ctx.get('previousIndex').push(ctx.get(polyline).length+1);
           // Only consider the first route
           ctx.set("distance", data.features[0].properties.summary[0].distance);
           ctx.set("duration", data.features[0].properties.summary[0].duration);
@@ -142,7 +142,9 @@ export default Component.extend({
           .then(function(data){
             ctx.set("firstAddress", data.display_name);
         });
-        ctx.send("addPolyline", e.latlng.lat, e.latlng.lng,0, "polyline");
+        ctx.send("addPolyline", e.latlng.lat, e.latlng.lng,0, "polylineRecommended");
+        ctx.send("addPolyline", e.latlng.lat, e.latlng.lng,0, "polylineFastest");
+        ctx.send("addPolyline", e.latlng.lat, e.latlng.lng,0, "polylineShortest");
         ctx.set("firstClick", true);
       } else if (!this.get("lastClick")){
         url = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + e.latlng.lat + "&lon=" + e.latlng.lng + "&zoom=18&addressdetails=1";
@@ -153,7 +155,9 @@ export default Component.extend({
           .then(function(data){
             ctx.set("secondAddress", data.display_name);
         });
-        ctx.send("pathCreation", e.latlng.lat, e.latlng.lng, preference, profile, "polyline");
+        ctx.send("pathCreation", e.latlng.lat, e.latlng.lng, preference, profile, "polylineRecommended");
+        ctx.send("pathCreation", e.latlng.lat, e.latlng.lng, "shortest", profile, "polylineShortest");
+        ctx.send("pathCreation", e.latlng.lat, e.latlng.lng, "fastest", profile, "polylineFastest");
       }
     },
 
@@ -165,13 +169,17 @@ export default Component.extend({
         fetch(url)
         .then(function(response) { return response.json(); })
         .then(function(data){
-          ctx.send("addPolyline", data[0].lat, data[0].lon,0), "polyline";
+          ctx.send("addPolyline", data[0].lat, data[0].lon, 0, "polylineRecommended");
+          ctx.send("addPolyline", data[0].lat, data[0].lon, 0, "polylineFastest");
+          ctx.send("addPolyline", data[0].lat, data[0].lon, 0, "polylineShortest");
           ctx.set("firstClick", true);
           ctx.set("lat", data[0].lat);
           ctx.set("lng", data[0].lon);
         });
       } else {
-        this.set("polyline", A([]));
+        this.set("polylineRecommended", A([]));
+        this.set("polylineFastest", A([]));
+        this.set("polylineShortest", A([]));
         this.set('firstClick', false);
         this.set('lastClick', false);
         ctx.send("searchFirstAddress");
@@ -189,10 +197,15 @@ export default Component.extend({
             let preference = ctx.get('preference');
             let profile = ctx.get('profile');
             // Calling ORS for a polyline segment joining the given two coordinates
-            ctx.send("pathCreation", data[0].lat, data[0].lon, preference, profile, "polyline");
+            ctx.send("pathCreation", data[0].lat, data[0].lon, preference, profile, "polylineRecommended");
+            ctx.send("pathCreation", data[0].lat, data[0].lon, "shortest", profile, "polylineShortest");
+            ctx.send("pathCreation", data[0].lat, data[0].lon, "fastest", profile, "polylineFastest");
+            ctx.set("lastClick", true);
           });
         } else {
-          ctx.set("polyline", A([ctx.get("polyline").firstObject]));
+          ctx.set("polylineRecommended", A([ctx.get("polylineRecommended").firstObject]));
+          ctx.set("polylineFastest", A([ctx.get("polylineFastest").firstObject]));
+          ctx.set("polylineShortest", A([ctx.get("polylineShortest").firstObject]));
           ctx.set("lastClick", false);
           ctx.send("searchSecondAddress");
         }
